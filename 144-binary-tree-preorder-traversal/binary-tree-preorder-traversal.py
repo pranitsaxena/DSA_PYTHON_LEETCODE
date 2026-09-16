b3cfb6643ba87ebadf00c494ec51504d9630b8ee
pranitsaxena <pranitsaxena_25@yahoo.com>
2026-09-16 21:19:14 +0530

    Time: 3 ms (2.96%) | Memory: 12.5 MB (40.46%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def preorderTraversal(self, root):
+        result = []
+
+        def preorder(node):
+            if not node:
+                return
+
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+
+        preorder(root)
+
+        return result
+        
